f9432fab6a2d7bcdcb5a6fd6d7020f03c386b707
v.peschanyi <v.peschanyi@corp.mail.ru>
2020-05-03 20:34:07 +0300

    - Logging

---
+++ NppPluginForHC/Log.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace NppPluginForHC
+{
+    public static class Log
+    {
+        private const string TraceFilePath = "C:/NppPluginForHC/TRACE.txt";
+        private const string ErrorFilePath = "C:/NppPluginForHC/ERROR.txt";
+
+        private static readonly string TracePId = Process.GetCurrentProcess().Id.ToString("X04");
+
+        [Conditional("TRACE_ALL")]
+        internal static void Trace(string msg)
+        {
+            Debug.WriteLine(msg);
+            try
+            {
+                using (TextWriter w = new StreamWriter(TraceFilePath, true))
+                {
+                    StackTrace stackTrace = new StackTrace();
+                    MethodBase methodBase = stackTrace.GetFrame(1).GetMethod();
+                    w.WriteLine(
+                        $"{DateTime.Now.ToLongTimeString()}:{DateTime.Now.Millisecond:000} <{TracePId}> {new String(' ', (stackTrace.FrameCount - 1) * 3)}[{methodBase.Name}] {msg}");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error while attempting to write trace file:\n" + ex.Message,
+                    "NppPluginForHC", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        internal static void ErrorOut(Exception ex)
+        {
+            try
+            {
+                using (TextWriter w = new StreamWriter(ErrorFilePath, true))
+                {
+                    w.WriteLine(
+                        $"\n{DateTime.Now.Year}-{DateTime.Now.Month:00}-{DateTime.Now.Day:00} {DateTime.Now.Hour:00}-{DateTime.Now.Minute:00}-{DateTime.Now.Second:00}:\n" +
+                        "====================");
+                    w.WriteLine(ex.Message);
+                    w.WriteLine(ex.StackTrace);
+                }
+
+                MessageBox.Show("Owing to unfortunate circumstances an error with the following message occured:\n\n"
+                                + "\"" + ex.Message + "\"\n\n"
+                                + "Hence a logfile has been written to the NppPluginForHC folder.\n"
+                                + "Please post its content in the forum, if you think it's worth being fixed.\n"
+                                + "Sorry for the inconvenience.",
+                    "NppPluginForHC", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error while attempting to write error logfile:\n" + e.Message,
+                    "NppPluginForHC", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
--- NppPluginForHC/Main.cs
+++ NppPluginForHC/Main.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
-using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
@@ -30,17 +28,25 @@ namespace NppPluginForHC
             }
         };
 
+        private static void GetVersion()
+        {
+            MessageBox.Show("Version: 0.0.2");
+        }
+
         public static void OnNotification(ScNotification notification)
         {
             switch (notification.Header.Code)
             {
                 case (uint) NppMsg.NPPN_READY:
+                    Log.Trace("NPPN_READY");
                     MouseHook.Start();
                     break;
                 case (uint) SciMsg.SCN_FOCUSIN:
+                    Log.Trace("SCN_FOCUSIN");
                     MouseHook.RegisterListener(OnLeftMouseClick);
                     break;
                 case (uint) SciMsg.SCN_FOCUSOUT:
+                    Log.Trace("SCN_FOCUSOUT");
                     MouseHook.CleanListeners();
                     break;
             }
@@ -51,11 +57,6 @@ namespace NppPluginForHC
             MouseHook.CleanListeners();
             MouseHook.Stop();
             // Win32.WritePrivateProfileString("SomeSection", "SomeKey", someSetting ? "1" : "0", iniFilePath);
-        }
-
-        private static void GetVersion()
-        {
-            MessageBox.Show("Version: 0.0.1");
         }
 
         internal static void CommandMenuInit()
@@ -73,6 +74,29 @@ namespace NppPluginForHC
             PluginBase.SetCommand(2, "GoToDefinition", GoToDefinition, new ShortcutKey(true, false, true, Keys.Enter));
         }
 
+        private static void GoToDefinition()
+        {
+            StringBuilder sbWord = new StringBuilder(4096);
+
+            if (Win32.SendMessage(PluginBase.nppData._nppHandle, NppMsg.NPPM_GETCURRENTWORD, 4096, sbWord) != IntPtr.Zero)
+            {
+                // NppMsg.NPPM_GETCURRENTDOCINDEX - документа (tab pane)
+                int currentLine = Win32.SendMessage(PluginBase.nppData._nppHandle, (uint) NppMsg.NPPM_GETCURRENTLINE, 0, 0).ToInt32();
+                int currentColumn = Win32.SendMessage(PluginBase.nppData._nppHandle, (uint) NppMsg.NPPM_GETCURRENTCOLUMN, 0, 0).ToInt32();
+
+                String word = sbWord.ToString();
+                if (sbWord.Length > 0)
+                {
+                    MessageBox.Show("word: " + word);
+                    return;
+                }
+            }
+
+            Win32.SendMessage(PluginBase.GetCurrentScintilla(), SciMsg.SCI_GRABFOCUS, 0, 0);
+            System.Media.SystemSounds.Hand.Play();
+        }
+
+        #region " Layout Base "
 
         internal static void myDockableDialog()
         {
@@ -111,30 +135,6 @@ namespace NppPluginForHC
             }
         }
 
-        private static void GoToDefinition()
-        {
-            StringBuilder sbWord = new StringBuilder(4096);
-
-            if (Win32.SendMessage(PluginBase.nppData._nppHandle, NppMsg.NPPM_GETCURRENTWORD, 4096, sbWord) != IntPtr.Zero)
-            {
-                // NppMsg.NPPM_GETCURRENTDOCINDEX - документа (tab pane)
-                int currentLine = Win32.SendMessage(PluginBase.nppData._nppHandle, (uint) NppMsg.NPPM_GETCURRENTLINE, 0, 0).ToInt32();
-                int currentColumn = Win32.SendMessage(PluginBase.nppData._nppHandle, (uint) NppMsg.NPPM_GETCURRENTCOLUMN, 0, 0).ToInt32();
-
-                String word = sbWord.ToString();
-                if (sbWord.Length > 0)
-                {
-                    MessageBox.Show("word: " + word);
-                    return;
-                }
-            }
-
-            Win32.SendMessage(PluginBase.GetCurrentScintilla(), SciMsg.SCI_GRABFOCUS, 0, 0);
-            System.Media.SystemSounds.Hand.Play();
-        }
-
-        #region " Layout Base "
-
         internal static void SetToolBarIcon()
         {
             toolbarIcons tbIcons = new toolbarIcons();
@@ -146,78 +146,5 @@ namespace NppPluginForHC
         }
 
         #endregion " Layout Base "
-
-        #region " Logging "
-
-        internal static void ErrorOut(Exception ex)
-        {
-            TRACE(ex.Message);
-            try
-
-            {
-                using (TextWriter w = new StreamWriter("C:\\NppPluginForHC.ERROR.txt", true))
-                {
-                    w.WriteLine(string.Format(
-                        "\n{0}-{1}-{2} {3}-{4}-{5}:\n" +
-                        "====================",
-                        DateTime.Now.Year,
-                        DateTime.Now.Month.ToString("00"),
-                        DateTime.Now.Day.ToString("00"),
-                        DateTime.Now.Hour.ToString("00"),
-                        DateTime.Now.Minute.ToString("00"),
-                        DateTime.Now.Second.ToString("00")));
-                    w.WriteLine(ex.Message);
-                    w.WriteLine(ex.StackTrace);
-                }
-
-                MessageBox.Show("Owing to unfortunate circumstances an error with the following message occured:\n\n"
-                                + "\"" + ex.Message + "\"\n\n"
-                                + "Hence a logfile has been written to the NppPluginForHC folder.\n"
-                                + "Please post its content in the forum, if you think it's worth being fixed.\n"
-                                + "Sorry for the inconvenience.",
-                    "NppPluginForHC", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-            catch (Exception e)
-            {
-                MessageBox.Show("Error while attempting to write error logfile:\n" + e.Message,
-                    "NppPluginForHC", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
-        [Conditional("TRACE_ALL")]
-        internal static void TRACE(string msg)
-        {
-            try
-            {
-                if (string.IsNullOrEmpty(TRACEPATH))
-                {
-                    TRACEPATH = Environment.ExpandEnvironmentVariables("C:\\NppPluginForHC.TRACE.txt");
-                    TRACEPID = Process.GetCurrentProcess().Id.ToString("X04");
-                }
-
-                using (TextWriter w = new StreamWriter(TRACEPATH, true))
-                {
-                    StackTrace stackTrace = new StackTrace();
-                    MethodBase methodBase = stackTrace.GetFrame(1).GetMethod();
-                    w.WriteLine(string.Format("{0}:{1:000} <{2}> {3}[{4}] {5}",
-                        DateTime.Now.ToLongTimeString(), DateTime.Now.Millisecond, TRACEPID,
-                        new String(' ', (stackTrace.FrameCount - 1) * 3), methodBase.Name, msg));
-                }
-            }
-            catch (Exception ex)
-            {
-                if (!TRACEERRORSHOWN)
-                {
-                    TRACEERRORSHOWN = true;
-                    MessageBox.Show("Error while attempting to write trace file:\n" + ex.Message,
-                        "NppPluginForHC", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-            }
-        }
-
-        static string TRACEPATH, TRACEPID;
-        static bool TRACEERRORSHOWN;
-
-        #endregion Logging
     }
 }
